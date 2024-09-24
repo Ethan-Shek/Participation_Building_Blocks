c0b8a273567c84e393335d74987651c52c096db9
Ethan-Shek <dinoeshek@gmail.com>
2024-09-24 09:24:26 -0400

    First backup

---
+++ Participation_Building_Blocks/Participation_Building_Blocks/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Participation_Building_Blocks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            bool likeCake = false;
+            string word = "Yippee";
+            double dec = 3.1415;
+            Console.WriteLine(num);
+            Console.WriteLine(likeCake);
+            Console.WriteLine(word);
+            Console.WriteLine(dec);
+
+        }
+
+
+
+    }
+}
